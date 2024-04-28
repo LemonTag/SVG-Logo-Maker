b0b1858e58d5a1382aa37a73c79aa93fb1895bc4
Keegan Micu <LemonTag73@gmail.com>
2024-04-28 13:37:42 -0400

    made other files for the shapes seperate for them

--- examples/Lib/circle.js
+++ examples/Lib/circle.js
@@ -1,4 +1,3 @@
-
 <svg
     width="200"
     height="200"
@@ -6,10 +5,5 @@
 >
     <circle cx="0" cy="20" r="70" fill="green" />
 
-    <rect x="-50" y="-50" rx="0" width="100" height="100" fill="green" />
-
-    <polygon points="-5,-10 -50, 100 60, 100" fill="green" />
-
     <text x="0" y="0" font-size="30" text-anchor="middle" fill="white">SVG</text>
 </svg>
-
--- examples/Lib/svg.js
+++ examples/Lib/svg.js
@@ -1,4 +1,4 @@
-<svg height="30" width="200" xmlns="http://www.w3.org/2000/svg">
+{/* <svg height="30" width="200" xmlns="http://www.w3.org/2000/svg">
 
     <text x="5" y="30" fill="white" font-size ="35"> SVG </text>
-</svg>
+</svg> */}
